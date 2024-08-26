89e388075bc9a3fe3d428a580fac48f08a57eece
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-08-26 11:55:52 -0300

    Update Case 1, 2

--- calculadora.c
+++ calculadora.c
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 
-// Função para converter decimal para binário
 void DecimalBinario(int n) {
     if (n < 0) {
         printf("Número inválido. Apenas números não-negativos são suportados.\n");
@@ -36,11 +35,42 @@ void DecimalBinario(int n) {
     printf("\n");
 }
 
+void DecimalOctal(int n) {
+    if (n < 0) {
+        printf("Número inválido. Apenas números não-negativos são suportados.\n");
+        return;
+    }
+
+    int octal[32];
+    int i = 0;
+
+    if (n == 0) {
+        printf("Resultado em Octal: 0\n");
+        return;
+    }
+
+    printf("Passo a passo da conversão de %d para octal:\n", n);
+
+    while (n > 0) {
+        octal[i] = n % 8;
+        printf("%d / 8 = %d, resto = %d\n", n, n / 8, octal[i]);
+        n = n / 8;
+        i++;
+    }
+
+    printf("Resultado em Octal: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", octal[j]);
+    }
+    printf("\n");
+}
+
 int main() {
     int opcao, num;
 
     printf("Calculadora Programador Didática\n");
     printf("1 - Converter base 10 para base 2\n");
+    printf("2 - Converter base 10 para base 8\n");
 
     printf("Escolha uma opção: ");
     scanf("%d", &opcao);
@@ -50,6 +80,11 @@ int main() {
             printf("Digite o número em base 10: ");
             scanf("%d", &num);
             DecimalBinario(num);
+            break;
+        case 2:
+            printf("Digite o número em base 10: ");
+            scanf("%d", &num);
+            DecimalOctal(num);
             break;
         default:
             printf("Opção inválida!\n");
