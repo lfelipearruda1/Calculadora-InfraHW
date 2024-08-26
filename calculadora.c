6949f9ebf2b58bf1f29910b0962906d2cc78ec07
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-08-26 11:58:24 -0300

    Update Case 1,2,3

--- calculadora.c
+++ calculadora.c
@@ -65,12 +65,48 @@ void DecimalOctal(int n) {
     printf("\n");
 }
 
+void DecimalHexadecimal(int n) {
+    if (n < 0) {
+        printf("Número inválido. Apenas números não-negativos são suportados.\n");
+        return;
+    }
+
+    char hexadecimal[32];
+    int i = 0;
+    int resto;
+
+    if (n == 0) {
+        printf("Resultado em Hexadecimal: 0\n");
+        return;
+    }
+
+    printf("Passo a passo da conversão de %d para hexadecimal:\n", n);
+
+    while (n > 0) {
+        resto = n % 16;
+        if (resto < 10)
+            hexadecimal[i] = '0' + resto;
+        else
+            hexadecimal[i] = 'A' + (resto - 10);
+        printf("%d / 16 = %d, resto = %c\n", n, n / 16, hexadecimal[i]);
+        n = n / 16;
+        i++;
+    }
+
+    printf("Resultado em Hexadecimal: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", hexadecimal[j]);
+    }
+    printf("\n");
+}
+
 int main() {
     int opcao, num;
 
     printf("Calculadora Programador Didática\n");
     printf("1 - Converter base 10 para base 2\n");
     printf("2 - Converter base 10 para base 8\n");
+    printf("3 - Converter base 10 para base 16\n");
 
     printf("Escolha uma opção: ");
     scanf("%d", &opcao);
@@ -85,6 +121,11 @@ int main() {
             printf("Digite o número em base 10: ");
             scanf("%d", &num);
             DecimalOctal(num);
+            break;
+        case 3:
+            printf("Digite o número em base 10: ");
+            scanf("%d", &num);
+            DecimalHexadecimal(num);
             break;
         default:
             printf("Opção inválida!\n");
