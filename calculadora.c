9d4c6dd5fa15392add79de2eaa1bbc4d3df422fd
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-08-28 10:47:16 -0300

    Update FINAL
    Ajuste do Case 6 e 7.
    Adicionei o Case 8 para dar o break.
    Adicionei o cabeçalho.

--- calculadora.c
+++ calculadora.c
@@ -1,11 +1,21 @@
 //Cabeçalho:
 //Aluno: Luiz Felipe Pessoa de Arruda
 //Terceiro Período B
-//(26/08/2024)
+//Questão 1a: (22/08/2024) - 08:24
+//Questão 1b: (22/08/2024) - 10: 50
+//Questão 1c: (23/08/2024) - 10:16
+//Questão 1d: (26/08/2024) - 9:40
+//Questão 2: (28/08/2024) - 08:32
+//Questão 3: (28/08/2024) - 10:18
 
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
+
+void converterParaBinario(unsigned long long valor, int tamanho) {
+    for (int i = tamanho - 1; i >= 0; i--) {
+        printf("%d", (valor >> i) & 1);
+    }
+}
 
 void DecimalBinario(int n) {
     if (n < 0) {
@@ -87,9 +97,9 @@ void DecimalHexadecimal(int n) {
     while (n > 0) {
         resto = n % 16;
         if (resto < 10)
-            hexadecimal[i] = 48 + resto;
+            hexadecimal[i] = '0' + resto;
         else
-            hexadecimal[i] = 55 + resto;
+            hexadecimal[i] = 'A' + (resto - 10);
         printf("%d / 16 = %d, resto = %c\n", n, n / 16, hexadecimal[i]);
         n = n / 16;
         i++;
@@ -156,21 +166,24 @@ void DecimalComplemento2(int n) {
     printf("\n");
 }
 
-void RealParaFloat(float num) {
+void RealFloat(float num) {
     unsigned int bits;
     memcpy(&bits, &num, sizeof(bits));
 
     int sinal = (bits >> 31) & 1;
     int expoente = (bits >> 23) & 0xFF;
-    int fracao = bits & 0x7FFFFF;
+    unsigned int fracao = bits & 0x7FFFFF;
 
     printf("Número: %f\n", num);
     printf("Sinal: %d\n", sinal);
-    printf("Expoente: %d (0x%X)\n", expoente - 127, expoente);
-    printf("Fração: 0x%X\n", fracao);
+    printf("Expoente: %d (0x%X) em binário: ", expoente - 127, expoente);
+    converterParaBinario(expoente, 8);
+    printf("\nFração em binário: ");
+    converterParaBinario(fracao, 23);
+    printf("\n");
 }
 
-void RealParaDouble(double num) {
+void RealDouble(double num) {
     unsigned long long bits;
     memcpy(&bits, &num, sizeof(bits));
 
@@ -180,66 +193,73 @@ void RealParaDouble(double num) {
 
     printf("Número: %lf\n", num);
     printf("Sinal: %d\n", sinal);
-    printf("Expoente: %d (0x%X)\n", expoente - 1023, expoente);
-    printf("Fração: 0x%llX\n", fracao);
+    printf("Expoente: %d (0x%X) em binário: ", expoente - 1023, expoente);
+    converterParaBinario(expoente, 11);
+    printf("\nFração em binário: ");
+    converterParaBinario(fracao, 52);
+    printf("\n");
 }
 
 int main() {
-    int opcao, num;
+    int opcao = 0, num;
     float fnum;
     double dnum;
+    while(opcao != 8) {
+        printf("Calculadora Programador Didática\n");
+        printf("1 - Converter base 10 para base 2\n");
+        printf("2 - Converter base 10 para base 8\n");
+        printf("3 - Converter base 10 para base 16\n");
+        printf("4 - Converter base 10 para Código BCD\n");
+        printf("5 - Converter base 10 para complemento de 2 com 16 bits\n");
+        printf("6 - Converter número real para formato float\n");
+        printf("7 - Converter número real para formato double\n");
+        printf("8 - Sair\n");
 
-    printf("Calculadora Programador Didática\n");
-    printf("1 - Converter base 10 para base 2\n");
-    printf("2 - Converter base 10 para base 8\n");
-    printf("3 - Converter base 10 para base 16\n");
-    printf("4 - Converter base 10 para Código BCD\n");
-    printf("5 - Converter base 10 para complemento de 2 com 16 bits\n");
-    printf("6 - Converter número real para formato float\n");
-    printf("7 - Converter número real para formato double\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
-        case 1:
-            printf("Digite o número em base 10: ");
-            scanf("%d", &num);
-            DecimalBinario(num);
-            break;
-        case 2:
-            printf("Digite o número em base 10: ");
-            scanf("%d", &num);
-            DecimalOctal(num);
-            break;
-        case 3:
-            printf("Digite o número em base 10: ");
-            scanf("%d", &num);
-            DecimalHexadecimal(num);
-            break;
-        case 4:
-            printf("Digite o número em base 10: ");
-            scanf("%d", &num);
-            DecimalBCD(num);
-            break;
-        case 5:
-            printf("Digite o número em base 10 (com sinal): ");
-            scanf("%d", &num);
-            DecimalComplemento2(num);
-            break;
-        case 6:
-            printf("Digite o número real: ");
-            scanf("%f", &fnum);
-            RealParaFloat(fnum);
-            break;
-        case 7:
-            printf("Digite o número real: ");
-            scanf("%lf", &dnum);
-            RealParaDouble(dnum);
-            break;
-        default:
-            printf("Opção inválida!\n");
-            return 1;
+        switch (opcao) {
+            case 1:
+                printf("Digite o número em base 10: ");
+                scanf("%d", &num);
+                DecimalBinario(num);
+                break;
+            case 2:
+                printf("Digite o número em base 10: ");
+                scanf("%d", &num);
+                DecimalOctal(num);
+                break;
+            case 3:
+                printf("Digite o número em base 10: ");
+                scanf("%d", &num);
+                DecimalHexadecimal(num);
+                break;
+            case 4:
+                printf("Digite o número em base 10: ");
+                scanf("%d", &num);
+                DecimalBCD(num);
+                break;
+            case 5:
+                printf("Digite o número em base 10 (com sinal): ");
+                scanf("%d", &num);
+                DecimalComplemento2(num);
+                break;
+            case 6:
+                printf("Digite o número real: ");
+                scanf("%f", &fnum);
+                RealFloat(fnum);
+                break;
+            case 7:
+                printf("Digite o número real: ");
+                scanf("%lf", &dnum);
+                RealDouble(dnum);
+                break;
+            case 8:
+                break;
+            default:
+                printf("Opção inválida!\n");
+                return 1;
+        }
     }
 
     return 0;
